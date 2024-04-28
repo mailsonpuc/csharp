d2c16fdc73c4be58c16813b1a25e40809c39d16f
mailsonpuc <mailson.costa@sga.pucminas.br>
2024-04-28 19:14:36 -0300

    atribuição de operadores

---
+++ 06_atribuir_operador/operador.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+ 
+      int x = 10;
+  
+
+     Console.WriteLine(x += 3);
+     Console.WriteLine(x -= 3);
+     Console.WriteLine(x *= 3);
+     Console.WriteLine(x /= 3);
+     Console.WriteLine(x %= 2);
+     Console.WriteLine(x &= 3);
+     Console.WriteLine(x |= 3);
+     Console.WriteLine(x ^= 3);
+     Console.WriteLine(x >>= 3);
+     Console.WriteLine(x <<= 3);
+    
+    
+
+
+
+
+
+
+
+    
+    }
+  }
+}
