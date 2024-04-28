78a4489a60830b72bff3e65582a0265c8c82f87c
mailsonpuc <mailson.costa@sga.pucminas.br>
2024-04-28 17:53:06 -0300

    comentarios

---
+++ 02_comentario/comentario.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace App
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      
+        //Isso é um comentario
+        Console.WriteLine("Hello");
+
+        //SOmando numeros
+        Console.WriteLine(5 + 5);
+    
+
+      }
+   }
+}
